9693a8376b90175700aaf5fd5771f33407843463
Mohamed Hilal <118242678+DePrince02@users.noreply.github.com>
2023-05-31 11:25:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,177 @@
+function photo()
+{
+ alert(` 
+ PHOTOGRAPHY COMPITITION :
+
+ 1. It is a solo event 
+
+ 2. Participants are required to bring their own digital cameras or Phone Camera take an photography your own photo without add any design.
+
+ 3. Number of photographs can vary from 1 to 5. 
+
+ 4. Participants are sending your photography through mail only, or send me link in multitalentcontest@gmail.com.
+
+ • Photographs should not include any kind of vulgarity or offensive matter. Any such activity might cause the disqualification of the participant. 
+ 
+ • Entries hall be judged on – creativity and theme or message portrayed
+ `);
+}
+function paint()
+{
+    alert(`PAINTING COMPITITION :
+
+    • Candidates should bring their own stationery material, colours etc. 
+
+    • Painting should be in A3 Art Board/Sheet.
+
+    • Water/Fabric colour can be used. 
+
+    • Poster Painting should be the depiction of cultural traditions of the language concert
+   
+   `);
+}
+function video()
+{
+    alert(`SHORT VIDEO COMPETITION
+
+1. It is a team-based event (3 - 4 members per team) 
+
+2. “N” no. of team per college 
+
+3. Video should be of 5 to 7 minutes that captures the theme essence 
+
+4. Participants are required to use digital video cameras 
+
+5. Can submit only one video as their final submission. 
+
+6. Participants should submit their video prior to the event • Video should not include any kind of vulgarity or offensive matter. Any such activity might cause the disqualification of the participant. • Direction, acting, cinematography will be given more importance
+`);
+}
+function music()
+{
+    alert(`
+    MUSIC INSTRUMENT PLAYING :
+
+    1. Solo event 
+
+    2. Participants must carry their own instruments 
+
+    3. Judging will be done based on – clarity of notes, rhythm, composition & innovation 
+
+    4. N no. of participants from per college.
+
+    5. Only 5 minutes event, 
+    `);
+}
+function speech()
+{
+    alert(`
+    SPEECH COMPITITION :
+    
+    1. While registering for speech competition please mention Speech topic in the comments section. 
+
+    2. Grace time:
+
+     Category a: (2 minutes + 1 Minute Grace).
+
+     Category b: 3 minutes + 1 Minute Grace, 
+
+     Category c: 4 minutes + 1 Minute Grace,
+
+     Category d:  5 minutes + 1 Minute Grace. 
+
+    3. No one should read from a written paper. If anyone does, he/she will not be evaluated by the judges. Notes will be allowed which is written on kids hand sized paper. 
+
+    4. The Speech competition is open to all students 
+
+       Judges' Decision is final.  Winners will be posted on AKC web site as early as possible. 
+
+       Colloquial form of speech is admitted. 
+
+       Usage of English words to the minimal is highly appreciated. All languages accepted. 
+    `)
+}
+function swrite()
+{
+    alert(`
+    STORY WRITING COMPITITION :
+
+    • Maximum time allowed is 20 minutes only (as per schedule) 
+
+    • Winners of the Essay Writing Competitions should submit the softcopy of the essay in Unicode font (Ms-word format) within two days of announcement of the results. Necessary skill training in computer in the respective language will be provided, for the students those who need training, in the lab of School of Humanities. 
+
+    • Topics of the essay writing competition are available in the schedule. No language restriction.`)
+}
+function dance()
+{
+    alert(`
+    DANCE COMPITITION :
+    
+    SOLO:
+
+    1. participants should bring their own cd’s or pen drives containing the sound track 
+
+    2. individuality and originality of the dance is encouraged 
+
+    3. use of prop in the dance is mandatory 
+
+    4. Time – 5 min 
+
+    GROUP: 
+
+    1. participants should bring their own cd’s or pen drives containing the sound track in standard format 
+
+    2. use of prop i.e., Hat is mandatory and theme selection is optional 
+    `)
+}
+function sing()
+{
+    alert(`
+    SINGING COMPITITION:
+
+    • Song should be on any of the cultural traditions of the language community. 
+
+    • Each participant will be allowed maximum five minutes.
+
+    SOLO & DUET:
+
+    10 no. of participants per college (entries for both solo and duet is separately) 
+    Time – 5 min (solo) - 	Time – 6 min (Duet)
+`)
+}
+function poem()
+{
+    alert(`
+    POEM WRITING COMPITITION  கவிதை போட்டி :
+    1.) Poetry – Must be a poem you have written. (The poem must not have been posted anywhere on any other website)
+                * (New Poetry, Haiku Poetry, Traditional Poetry)
+
+    2.) Tamil Poems - Love, Friendship, Motherhood, Nature, Family, Life, Politics etc. is good.
+
+    3.) If your poem is good then your work will be featured on our site within 60 hours of submission.
+
+    4.) Minimum Poem Lines: Four lines
+
+    5.) Maximum Poem Lines: Forty Lines
+
+    6.) Winners - The prize will be awarded based on the appreciation and criticism received by the poem.
+`)
+}
+function talent()
+{
+    alert(`
+    PERSONAL OWN TALENT CONTEST :
+
+    1. Solo event 
+
+    2. Participants must carry their own instruments 
+
+    3. Judging will be done based on – Talent, Accuracy, composition & innovation 
+
+    4. Only one Talent should be shown this category at a time of registration, if you are will you can be allowed to participate other events, but this contest one should be shown.
+
+    5. Only 5 minutes event, 
+
+    6. Judges Decision final.
+`)
+}
